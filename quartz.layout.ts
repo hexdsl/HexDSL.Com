f76bae0cc7a01127f783e05293dd6c1851d60c45
HexDSL <Hexdsl@gmail.com>
2024-02-16 20:03:36 +0000

    Quartz sync: Feb 16, 2024, 8:03 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "YouTube": "https://www.youtube.com/hexdsl",
+      "Join my Discord": "https://discord.com/invite/37GYAhj",
     },
   }),
 }
